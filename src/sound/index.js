40ffc39640492ee99eb00e8acce12a45210bcb6e
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-12-12 14:17:03 -0500

    slim import

--- src/sound/index.js
+++ src/sound/index.js
@@ -3,7 +3,7 @@ export * from "./SoundPlayer.js";
 export * from "./TonePlayer.js";
 export * from "./TrackPlayer.js";
 
-export * from "./AudioClip.js";
-export * from "./AudioClipPlayer.js";
-export * from "./Microphone.js";
+// export * from "./AudioClip.js";
+// export * from "./AudioClipPlayer.js";
+// export * from "./Microphone.js";
 // export * from './Transcriber.js';
